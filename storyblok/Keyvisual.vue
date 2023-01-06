66ae40c245fda1a60ed4aa4a388f6508faf5cac7
TwoBee <mail@tobiaszimmer.dev>
2023-01-06 19:35:08 +0100

    add: dynamic keyvisual

--- storyblok/Keyvisual.vue
+++ storyblok/Keyvisual.vue
@@ -1,41 +1,58 @@
 <template>
-  <section
-    v-editable="blok">
-  <img :src="blok.image.filename" />
-  <h2>{{blok.headline}}</h2>
-  <h3>{{blok.subline}}</h3>
+  <section class="keyvisual" v-editable="blok">
+    <!-- <img :src="blok.image.filename" /> -->
+    <div class="text__wrapper">
+      <h2>{{ blok.headline }}</h2>
+      <h3>{{ blok.subline }}</h3>
+      <h3>{{ blok.image.filename }}</h3>
+    </div>
   </section>
 </template>
 
 <script setup>
-defineProps({ blok: Object });
+  const props = defineProps({ blok: Object });
+  const backImage = ref("url(" +
+    props.blok.image.filename +
+    ")");
 </script>
 
 <style scoped lang="scss">
-  section{
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-  }
-  img{
-    width: 100vw;
-    height: calc(100vh - 8vw);
-    object-fit: cover;
-  }
-  h2{
+$color_white: red;
+
+.keyvisual {
+  background-image: v-bind('backImage');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+  height: 100vh;
+
+  .text__wrapper {
     position: absolute;
-    left: 4vw;
-    bottom: 20vw;
-    color: $color_white;
-    padding: 1rem;
-    @include font-headline-2;
+    background-color: blue;
+    min-height: 200px;
+    min-width: 400px;
   }
-  h3{
-    position: absolute;
-    left: 4vw;
-    bottom: 18vw;
-    color: $color_white;
-    padding: 1rem;
-    @include font-headline-3
-  }
+}
+
+img {
+  width: 100vw;
+  max-height: calc(100vh - 8vw);
+
+  object-fit: cover;
+}
+
+h2 {
+  position: absolute;
+  color: $color_white;
+  padding: 1rem;
+  @include font-headline-2;
+}
+
+h3 {
+  position: absolute;
+  color: $color_white;
+  padding: 1rem;
+  @include font-headline-3
+}
 </style>
