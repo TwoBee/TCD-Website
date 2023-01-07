0b1268b34de964c9af4adb1ed04821274e9b53ea
TwoBee <mail@tobiaszimmer.dev>
2023-01-07 23:22:09 +0100

    fix: keyvisual fonts and img height

--- storyblok/Keyvisual.vue
+++ storyblok/Keyvisual.vue
@@ -10,35 +10,16 @@
 
 <script setup>
   const props = defineProps({ blok: Object });
-  const backImage = ref("url(" +
+  const backgroundImage = ref("url(" +
     props.blok.image.filename +
     ")");
-  const greyscale = props.blok.greyscale ? 'grayscale(1)':'grayscale(0)';
-  console.log(greyscale);
+  const greyscale = props.blok.greyscale ? 'grayscale(1)' : 'grayscale(0)';
+  const backgroundAlignment = props.blok.alignment ? props.blok.alignment : 'center';
   const filter = ref(greyscale);
+
 </script>
 
 <style scoped lang="scss">
-.keyvisual {
-  background-image: v-bind('backImage');
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  position: relative;
-  height: calc(100vh - 8vw);
-  filter: v-bind('filter');
-
-  .text__wrapper {
-    position: absolute;
-    min-height: 200px;
-    min-width: 400px;
-    top: 60%;
-    left: 10%;
-    display: flex;
-    flex-direction: column;
-  }
-}
-
 img {
   width: 100vw;
   max-height: calc(100vh - 8vw);
@@ -47,12 +28,31 @@ img {
 }
 
 h2 {
-  color: $color_white;
   @include font-headline-2;
+  color: $color_white;
 }
 
 h3 {
+  @include font-headline-3;
   color: $color_white;
-  @include font-headline-3
+}
+
+.keyvisual {
+  background-image: v-bind('backgroundImage');
+  background-position: v-bind('backgroundAlignment');
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+  height: calc(100vh - 8vw);
+  filter: v-bind('filter');
+
+  .text__wrapper {
+    position: absolute;
+    min-width: 400px;
+    top: 60%;
+    left: 10%;
+    display: flex;
+    flex-direction: column;
+  }
 }
 </style>
