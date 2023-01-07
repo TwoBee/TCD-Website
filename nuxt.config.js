d5ab9a520a31831939c0769838debd21daf700b5
TwoBee <mail@tobiaszimmer.dev>
2023-01-07 23:22:34 +0100

    fix: anon config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,28 +1,26 @@
 export default defineNuxtConfig({
-  css: [
-    '@fortawesome/fontawesome-svg-core/styles.css'
-  ],
-  modules: [
-    [
-      "@storyblok/nuxt",
-      {
-        accessToken: "aYqrKUfxMJclVVvrCuCL3Qtt",
-      },
-    ],
-    "@nuxtjs/tailwindcss",
-  ],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          additionalData: '@import "@/assets/css/main.scss";',
-        },
-      },
-    },
-  },
-  nitro: {
-    prerender: {
-      routes: ['/404.html']
-    }
- }
+	css: ["@fortawesome/fontawesome-svg-core/styles.css"],
+	modules: [
+		[
+			"@storyblok/nuxt",
+			{
+				accessToken: process.env.STORYBLOK_TOKEN,
+			},
+		],
+		"@nuxtjs/tailwindcss",
+	],
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: '@import "@/assets/css/main.scss";',
+				},
+			},
+		},
+	},
+	nitro: {
+		prerender: {
+			routes: ["/404.html"],
+		},
+	},
 });
