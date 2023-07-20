1a80816a0d32532e51afef95c396bb80701f3e3b
TwoBee <mail@tobiaszimmer.dev>
2023-07-20 10:53:24 +0200

    refactor: :boom: Desktop now looks nice, but mobile is broken

--- storyblok/call-to-action.vue
+++ storyblok/call-to-action.vue
@@ -1,6 +1,6 @@
 <template>
-    <section>
-        <div class="cta--container" :data-testcontent="displayText" :class="{ mblock: offsetvalue === '0vh' }"
+    <section :data-backdroptext="displayText" >
+        <div class="cta--container" :class="{ mblock: offsetvalue === '0vh' }"
             :style="{ ' --offsetvar': offsetvalue }">
             <div class="cta--content">
                 <h2>{{ blok.Headline }}</h2>
@@ -18,7 +18,7 @@ import { useNow } from '@vueuse/core';
 
 const props = defineProps({ blok: Object });
 const currentYear = useNow();
-const currentYearString = "1970 \n\r " + currentYear.value.getFullYear()
+const currentYearString = "1970 " + currentYear.value.getFullYear()
 const displayText = ref(currentYearString);
 
 const offsetvalue = computed(() => {
@@ -32,39 +32,61 @@ const offsetvalue = computed(() => {
     margin: 10vh 0;
 }
 
-.cta--container {
-    width: 50%;
-    margin: 0 auto;
-    padding: 5%;
-    background-color: $color-secondary;
-    transform: translateY(v-bind(offsetvalue));
+section {
+    &::after {
+        content: attr(data-backdroptext);
+        position: absolute;
+        @include font-body-sans-b;
+        font-size: 9rem;
+        line-height: 7rem;
+        left: 0;
+        max-width: 100vw;
+        opacity: 0.1;
+        z-index: 1;
 
-    .cta--content {
-        display: grid;
-        gap: 1.2rem;
-        color: $color-accent;
+        @include desktop {
+            font-size: 240px;
+            line-height: 9rem;
+            top: 31%;
+            left: 10%;
 
-        h2 {
-            @include font-headline-serif-2;
-            letter-spacing: 0.2rem;
-        }
-
-        p,
-        a {
-            @include font-body-sans-r;
-            color: inherit;
         }
     }
 
-    &::after {
-        content: attr(data-testcontent);
-        @include font-body-sans-b;
-        font-size: 240px;
-        position: absolute;
-        line-height: 9rem;
-        top: 31%;
-        left: 10%;
-        opacity: 0.1;
+    .cta--container {
+        width: 100%;
+        background-color: $color-secondary;
+        padding: 5%;
+
+
+        @include desktop {
+            width: 50%;
+            margin: 0 auto;
+            transform: translateY(v-bind(offsetvalue));
+        }
+
+        .cta--content {
+            position: relative;
+            z-index: 2;
+            display: grid;
+            gap: 1.2rem;
+            color: $color-accent;
+
+            h2 {
+                @include font-headline-serif-2;
+
+
+                @include desktop {
+                    letter-spacing: 0.2rem;
+                }
+            }
+
+            p,
+            a {
+                @include font-body-sans-r;
+                color: inherit;
+            }
+        }
     }
 }
 </style>
