58a04396e04c07221e309ed3e3076aebb7f067bb
TwoBee <mail@tobiaszimmer.dev>
2023-07-30 16:24:08 +0200

    fix: :bug: fixing style of contactform

--- storyblok/Contactform.vue
+++ storyblok/Contactform.vue
@@ -1,19 +1,29 @@
 <template>
   <section>
     <FormKit type="form" @submit="sendMail" incomplete-message="Bitte vervollständigen Sie das Formular um fortzufahren.">
-      <FormKit type="text" name="name" id="name" label="Vorname" placeholder="Jon" validation="required"
-        :validation-messages="{
-          required: 'Dieses Feld ist pflicht.'
-        }" v-model="vorname" />
-      <FormKit type="text" name="name" id="name" label="Nachname" placeholder="Doe" validation="required"
-        v-model="nachname" />
-      <FormKit type="email" label="Email" validation="required|email"  :validation-messages="{
-        email: 'Bitte geben Sie eine gültige Adresse an.'
-      }" placeholder="jon.doe@mail.com" v-model="mail" />
-      <FormKit type="text" name="name" id="name" label="Straße und Hausnummer" placeholder="Musterstraße 1a"
-        v-model="anschrift" />
-      <FormKit type="text" name="name" id="name" label="Postleitzahl" placeholder="0815" v-model="plz" />
-      <FormKit type="text" name="name" id="name" label="Ort" placeholder="Musterstadt" v-model="ort" />
+      <div>
+        <FormKit type="text" outer-class="half" name="name" id="name" label="Vorname" placeholder="Jon"
+          validation="required" :validation-messages="{
+            required: 'Dieses Feld ist Pflicht.'
+          }" v-model="vorname" />
+        <FormKit type="text" class="half" name="name" id="name" label="Nachname" placeholder="Doe" validation="required"
+          v-model="nachname" />
+      </div>
+      <div>
+        <FormKit type="email" class="half" label="Email" validation="required|email" :validation-messages="{
+          email: 'Bitte geben Sie eine gültige Adresse an.'
+        }" placeholder="jon.doe@mail.com" v-model="mail" />
+      </div>
+      <div>
+        <FormKit type="text" name="name" id="name" label="Straße und Hausnummer" placeholder="Musterstraße 1a"
+          v-model="anschrift" />
+        <FormKit type="text" name="name" id="name" label="Postleitzahl" placeholder="0815" v-model="plz" />
+        <FormKit type="text" name="name" id="name" label="Ort" placeholder="Musterstadt" v-model="ort" />
+      </div>
+      <div>
+        <FormKit type="file" label="Antrag" accept=".pdf" help="Unterschriebener Antrag hochladen." multiple="false"
+          aria-placeholder="Keine Datei ausgewählt." />
+      </div>
     </FormKit>
   </section>
 </template>
@@ -44,16 +54,23 @@ section {
 
   :deep(form) {
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: 10px;
-    width: 50%;
+    flex-wrap: wrap;
+    width: min(50%, 800px);
     margin: 0 auto;
+    gap: 10px;
 
     >div {
-      width: 50%;
-      margin: 0 auto;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      flex: 0 0 100%;
+      gap: 10px;
+
+      >div {
+        flex: 0 0 45%;
+      }
     }
   }
 }
 </style>
+
